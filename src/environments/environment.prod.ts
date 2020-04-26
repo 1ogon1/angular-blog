68c5a1a2d164db139edadb5ca3c00f56bfb851c9
1ogon1 <konovalenkoruslan@gmail.com>
2020-04-26 21:52:32 +0300

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  fbDbUrl: 'https://rkonoval-blog.firebaseio.com'git remote,
+  fbDbUrl: 'https://rkonoval-blog.firebaseio.com',
   apiKey: 'AIzaSyAxbTEvp7KRQEH7UM7JfyfJ7Hz7hONnxNo'
 };

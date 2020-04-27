a7712696271483962cb2693d25c4bedb123c4887
Ruslan <konovalenkoruslan@gmail.com>
2020-04-27 11:15:22 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import {Environment} from "./interface";
 export const environment: Environment = {
   production: true,
   fbDbUrl: 'https://rkonoval-blog.firebaseio.com',
-  apiKey: 'AIzaSyAxbTEvp7KRQEH7UM7JfyfJ7Hz7hONnxNo'
+  apiKey: ''
 };
